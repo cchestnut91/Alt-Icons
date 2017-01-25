4927f4f8a07b45b2e4c4dce36f9e93d195992403
Calvin Chestnut <calvinchestnut@calvins-macbook-escape.local>
2017-01-25 17:39:08 -0500

    Initial Commit

---
+++ Alt Icons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alt Icons
+//
+//  Created by Calvin Chestnut on 1/25/17.
+//  Copyright © 2017 Calvin Chestnut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
